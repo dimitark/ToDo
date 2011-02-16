5f1f7e81ee049dbc5793ed0e7756ccefbcd17a14
Dimitar Kotevski <dime.kotevski@gmail.com>
2011-02-16 15:52:08 +0100

    Created the class Task that holds the data for a task.

---
+++ src/dime/android/todo/logic/Task.java
@@ -0,0 +1,67 @@
+package dime.android.todo.logic;
+
+public class Task
+{
+	private int			id;
+	private String		name;
+	private int			priority;
+	private boolean	completed;
+
+
+	public Task (int id, String name, int priority, boolean completed)
+	{
+		this.id = id;
+		this.name = name;
+		this.priority = priority;
+		this.completed = completed;
+	}
+
+
+	public int getId ( )
+	{
+		return id;
+	}
+
+
+	public void setId (int id)
+	{
+		this.id = id;
+	}
+
+
+	public String getName ( )
+	{
+		return name;
+	}
+
+
+	public void setName (String name)
+	{
+		this.name = name;
+	}
+
+
+	public int getPriority ( )
+	{
+		return priority;
+	}
+
+
+	public void setPriority (int priority)
+	{
+		this.priority = priority;
+	}
+
+
+	public boolean isCompleted ( )
+	{
+		return completed;
+	}
+
+
+	public void setCompleted (boolean completed)
+	{
+		this.completed = completed;
+	}
+
+}
