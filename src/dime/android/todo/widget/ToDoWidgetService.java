49db6b1ed69501c8b031a9309514f1db40e9fcb3
Dimitar Kotevski <dime.kotevski@gmail.com>
2011-03-19 20:07:26 +0100

    Bug fix (widget)

--- src/dime/android/todo/widget/ToDoWidgetService.java
+++ src/dime/android/todo/widget/ToDoWidgetService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import dime.android.todo.R;
 import dime.android.todo.db.DatabaseHelper;
 import dime.android.todo.logic.Task;
-import dime.android.todo.logic.TaskListAdapter;
 import dime.android.todo.ui.ToDoList;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -14,26 +13,43 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.view.View;
 import android.widget.RemoteViews;
 
 public class ToDoWidgetService extends Service
 {
 	private static final int[] task_names = {R.id.task_name_0, R.id.task_name_1, R.id.task_name_2, R.id.task_name_3, R.id.task_name_4, R.id.task_name_5};
-	private static final int[] task_color = {R.id.priority_color_0, R.id.priority_color_1, R.id.priority_color_2, R.id.priority_color_3, R.id.priority_color_4, R.id.priority_color_5};
+	private static final int[][] task_color = {{R.id.priority_color_0_low, R.id.priority_color_1_low, R.id.priority_color_2_low, R.id.priority_color_3_low, R.id.priority_color_4_low, R.id.priority_color_5_low},
+												{R.id.priority_color_0_normal, R.id.priority_color_1_normal, R.id.priority_color_2_normal, R.id.priority_color_3_normal, R.id.priority_color_4_normal, R.id.priority_color_5_normal},
+												{R.id.priority_color_0_high, R.id.priority_color_1_high, R.id.priority_color_2_high, R.id.priority_color_3_high, R.id.priority_color_4_high, R.id.priority_color_5_high}};
 	
 	private static final int WIDGET_ITEMS_COUNT = 6;
 	
 	
 	private DatabaseHelper dh;
 
-	private void setPriorityColor (RemoteViews rv, int viewId, int priority)
+	private void setPriorityColor (RemoteViews rv, int index, int priority)
 	{
-		rv.setInt (viewId, "setBackgroundResource", TaskListAdapter.colors[priority]);
+		for (int i = 0; i < 3; i++)
+		{
+			if (i == priority)
+			{
+				rv.setViewVisibility (task_color[i][index], View.VISIBLE);
+			}
+			else
+			{
+				rv.setViewVisibility (task_color[i][index], View.GONE);
+			}
+			
+		}
 	}
 	
-	private void clearColor (RemoteViews rv, int viewId)
+	private void clearColor (RemoteViews rv, int index)
 	{
-		rv.setInt (viewId, "setBackgroundResource", R.color.transparent);
+		for (int i = 0; i < 3; i++)
+		{
+			rv.setViewVisibility (task_color[i][index], View.GONE);
+		}
 	}
 	
 	private RemoteViews buildUpdate (Context context)
@@ -57,7 +73,7 @@ public class ToDoWidgetService extends Service
 		 */
 		for (int i = 0; i < Math.min (WIDGET_ITEMS_COUNT, uncompletedTasks.size ( )); i++){
 			updateViews.setTextViewText (task_names[i], uncompletedTasks.get (i).getName ( ));
-			setPriorityColor (updateViews, task_color[i], uncompletedTasks.get (i).getPriority ( ));
+			setPriorityColor (updateViews, i, uncompletedTasks.get (i).getPriority ( ));
 		}
 		
 		/*
@@ -66,7 +82,7 @@ public class ToDoWidgetService extends Service
 		for (int i = Math.min (WIDGET_ITEMS_COUNT, uncompletedTasks.size ( )); i < WIDGET_ITEMS_COUNT; i++ )
 		{
 			updateViews.setTextViewText (task_names[i], "");
-			clearColor(updateViews, task_color[i]);
+			clearColor(updateViews, i);
 		}
 		
 		return updateViews;
