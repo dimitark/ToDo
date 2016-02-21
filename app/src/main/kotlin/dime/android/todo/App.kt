3c7bf941d62b5134c7e897cd36f8d1768d60b2aa
Dimitar Kotevski <d.kotevski@sportradar.com>
2016-02-21 16:39:55 +0100

    Added Fabric.io/Crashlytics

--- app/src/main/kotlin/dime/android/todo/App.kt
+++ app/src/main/kotlin/dime/android/todo/App.kt
@@ -2,12 +2,16 @@ package dime.android.todo
 
 import android.app.Application
 import android.content.Intent
+import com.crashlytics.android.Crashlytics
 import dime.android.todo.widget.ToDoWidgetService
+import io.fabric.sdk.android.Fabric
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
+
         updateWidget()
     }
 
